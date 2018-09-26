d8dbbed24939ff3e37fd5e80dc15271a2c2b6f3f
bharathp9 <pbharathkumar86@gmail.com>
2018-09-26 13:11:46 +0800

    this is to commit for step4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './step2/App';
+import App from './step4/Login';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
